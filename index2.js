a5e762647a9704173de80d13dec70e14f5336b6b
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-18 15:40:17 -0600

    sam and sarah

--- index2.js
+++ index2.js
@@ -1,52 +1,57 @@
+
+
+//API 
 const fetchAPIurl = 'https://the-trivia-api.com/api/questions'
 
-
+//questions
 const questionContainer = document.querySelector("#questions-cont")
 const questionBar = document.querySelector("#question")
-const scoreDisplay = document.querySelector("#score")
 
-//controls div 
-const startBtn = document.querySelector("#start")
+
+// //score display 
+const scoreDisplay = document.querySelector("#score")
+scoreDisplay.classList.add('counter-display')
+
+
+
+//controls div - 
 const nextBtn = document.querySelector("#next")
 const resetBtn = document.querySelector("#reset")
 
-startBtn.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    console.log(e.type)
-})
+
+//counters 
+const counter = document.createElement('h1')
+
+
+// const counterButton = document.createElement('button')
+// counterButton.classList.add('counter-plus')
+// let counterPlusElement = document.querySelector('.counter-plus')
+
+
+//not working yet 
+function updateDisplay(){
+    let count = 0
+    count++
+    scoreDisplay.textContent = count
+}
 
 
 
 
 
+//answer buttons
 const buttonsDiv = document.querySelector("#answer-buttons")
-buttonsDiv.addEventListener('click', handleClick)
-function handleClick(e){
-    e.preventDefault()
-    if(e.target.id == 'true'){
-        console.log(e.target)
-    }
-}
 
 
-
-function incrementCounter(){
-    let i = 0
-    document.getElementById('true').value = ++i
-    console.log(i)
-}
 
 
 fetch(fetchAPIurl)
 .then(r=>r.json())
 .then(quiz => renderQuiz(quiz))
 
-
-
-
-function renderQuiz(quiz){
+function renderQuiz(quiz){   
     let i = 0
-    
+   
     const question = quiz[i].question
     questionBar.textContent = `${question}`
     console.log(question)
@@ -56,24 +61,20 @@ function renderQuiz(quiz){
     const incorrect = quiz[i].incorrectAnswers
     
     const totalArray = incorrect.concat(correct)
-    
-    totalArray.forEach(answer => {
+    const random = totalArray.sort(() => (Math.random() > .5) ? 1 : -1);
+
+    random.forEach(answer => {
         let answerButtons = document.createElement('button')
         answerButtons.type = 'button'
         answerButtons.textContent = answer
         answerButtons.classList.add('btn')
         buttonsDiv.append(answerButtons)
-
-    })           
+        answerButtons.addEventListener('click', ()=>{
+            if(answerButtons.textContent == correct){
+                updateDisplay()
+            } else {
+                alert('ya dummy')
+            }
+        })
+    })
 }
-    
-
-
-
-
-    
-
-
-
-    
-   
--- index3.js
+++ index3.js
@@ -1,21 +1,62 @@
-const game = document.querySelector("#game")
-const score = document.querySelector("#score")
+function make2DArray(cols, rows){
+	let array = new Array(cols);
+	for (let i = 0; i < array.length; i++){
+		array[i] = new Array(rows);
+	}
+	return array;
+}
+let grid;
+let cols;
+let rows;
+let resolution = 20;
 
-let question;
-function addQuestion(){
-    const row = document.createElement('div')
-    row.classList.add('categories')
-    game.append(row)
-
-
-    question.forEach(question => {
-        fetch('https://the-trivia-api.com/api/questions')
-            .then(r=>r.json())
-            .then(question => console.log(question))
-    })
+function setup() {
+	createCanvas(600, 400);
+	rows = height / resolution;
+	cols = width / resolution;
+	grid = make2DArray(cols, rows);
+		for (let i = 0; i < cols; i++){
+			for (let j = 0; j < rows; j++){
+				grid[i][j] = floor(random(2));
+		}
+	}
 }
 
 
-addQuestion()
+function draw() {
+	background(0);
+
+
+
+	for (let i = 0; i < cols; i++){
+		for (let j = 0; j < rows; j++){
+			let x = i * resolution;
+			let y = j * resolution;
+			if (grid[i][j] === 1){
+				fill(255);
+				stroke(0);
+			rect(x, y, resolution, resolution);
+			}
+		}
+	}
+}
+
+let next = make2DArray(cols,rows);
+for (let i = 0; i < cols; i++){
+	for (let j = 0; j < rows; j++){
+		let sum = 0;
+		let neighbors = count(grid, i, j);
+		// sum += grid[i-1][j];//1: DOWN
+		// sum += grid[i-1][j-1];//2: DOWN LEFT
+		// sum += grid[i][j-1];//3: LEFT
+		// sum += grid[i+1][j+1];//4: UP LEFT
+		// sum += grid[i+1][j];//5: UP
+		// sum += grid[i+1][j+1];//6: UP RIGHT
+		// sum += grid[i][j+1];//7:RIGHT
+		// sum += grid[i-1][j+1];//8: DOWN RIGHT
+		
+	}
+}
+grid = next;
 
 
